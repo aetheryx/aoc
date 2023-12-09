cbc8f722818167192e149c04789a6527436fa162
aetheryx <mhzy.ali@gmail.com>
2023-12-09 06:29:47 +0100

    zzzz

---
+++ src/2023/day9/part1.ts
@@ -0,0 +1,47 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { DayOpts } from 'src/day';
+
+export function day(input: string): any {
+  const lines = input.split('\n');
+  let sum = 0;
+  const histories: number[][] = [];
+
+  for (const line of lines) {
+    const row = line.split(' ').map(s => Number(s));
+    histories.push(row);
+  }
+
+  for (const history of histories) {
+    let levels: number[][] = [history];
+    const prev = () => levels.at(-1);
+    
+    while (prev().some(v => v !== 0)) {
+      const nextLevel = Array.from({ length: prev().length - 1 }, (_, i) => {
+        return prev()[i + 1] - prev()[i]
+      });
+
+      levels.push(nextLevel);
+    }
+
+    console.log('pre', levels);
+
+    prev().push(0);
+
+    for (let i = levels.length - 2; i >= 0; i--) {
+      const prev = levels[i + 1];
+      levels[i].push(levels[i].at(-1) + prev.at(-1));
+      console.log(i, levels);
+    }
+
+    sum += levels[0].at(-1);
+  }
+
+  return sum;
+}
+
+export const opts: DayOpts = {
+  // input: readFileSync(join(__dirname, './input.txt')).toString(),
+  sampleInput: readFileSync(join(__dirname, './sample.txt')).toString(),
+  sampleAnswer: 0,
+};
